45ddbb65b2423c0e4853138c75f1908b78d4918d
Ulrich Strauss <ulrich.strauss@gmail.com>
2013-01-02 15:58:03 +0100

    Git klappt

--- src/com/example/reifegrad/Main.java
+++ src/com/example/reifegrad/Main.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class Menu extends Activity {
+public class Main extends Activity {
     // Test
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {

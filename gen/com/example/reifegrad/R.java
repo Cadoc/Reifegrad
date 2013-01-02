d15e53390bec50a9d60cd888a01ee693f616a65a
Ulrich Strauss <ulrich.strauss@gmail.com>
2013-01-02 16:46:29 +0100

    Bilderauswahl Gallery Test 1

--- gen/com/example/reifegrad/R.java
+++ gen/com/example/reifegrad/R.java
@@ -14,7 +14,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int button1=0x7f070000;
+        public static final int button2=0x7f070002;
+        public static final int menu_settings=0x7f070003;
+        public static final int textView1=0x7f070001;
     }
     public static final class layout {
         public static final int activity_menu=0x7f030000;
@@ -23,8 +26,10 @@ public final class R {
         public static final int activity_menu=0x7f060000;
     }
     public static final class string {
+        public static final int Camerabutton=0x7f040003;
+        public static final int Picturebutton=0x7f040004;
+        public static final int Welcometext=0x7f040001;
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
     }
     public static final class style {
--- src/com/example/reifegrad/Main.java
+++ src/com/example/reifegrad/Main.java
@@ -1,15 +1,49 @@
 package com.example.reifegrad;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class Main extends Activity {
-    // Test
+	// Test
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		// Layout.xml Auswählen
 		setContentView(R.layout.activity_menu);
+
+		// Buttons referenzieren
+		Button buttonTakeImage = (Button) findViewById(R.id.button1);
+		Button buttonLoadImage = (Button) findViewById(R.id.button2);
+
+		// ClickListener erstellen
+		buttonLoadImage.setOnClickListener(new Button.OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				Intent intent = new Intent(
+						Intent.ACTION_PICK,
+						android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+				startActivityForResult(intent, 0);
+			}
+		});
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		if (resultCode == RESULT_OK) {
+			// URI des Bildes
+			Uri targetUri = data.getData();
+			// Aufruf Vergleichscode
+
+		}
 	}
 
 	@Override
